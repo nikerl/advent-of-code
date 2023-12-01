c5420e1717c377c0c45569c8423d0be8267b0702
Nik Erlandsson <nik.erlandsson@gmail.com>
2023-12-02 00:10:49 +0100

    Day 13 & 15

---
+++ AOC2022/day13.java
@@ -0,0 +1,133 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class day13 {
+    public static void main(String[] args) {
+        ArrayList<String> packets = readInputFile();
+
+
+
+    }
+
+
+    static ArrayList<String> readInputFile() {
+        ArrayList<String> strings = new ArrayList<String>();
+
+        try {
+            // read input file
+            File file = new File("input/day12_input.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                // add each line to a list of pairs
+                String row = scanner.nextLine();
+                strings.add(row);
+            }
+            scanner.close();
+        // if file couldnt be read print out error mgs
+        } catch (FileNotFoundException e) {
+            System.out.println("File could not be read.");
+            e.printStackTrace();
+        }
+
+        return strings;
+    }
+
+
+    static Pair parsePair(String[] inputs) {
+        Pair pair = new Pair();
+
+        for (int i = 0; i < inputs.length; i++) {
+            String input = inputs[i];
+            ArrayList<ArrayList<Integer>> packet = pair.packets.get(i);
+            int arrayIndex = 0;
+
+            for (int j = 0; j < input.length(); j++) {
+                char c = input.charAt(j);
+                if (c == '[') {
+                    pair.packets.get(i).add(new ArrayList<Integer>());
+                    continue;
+                }
+                if (c == ']') {
+                    arrayIndex++;
+                    continue;
+                }
+                while (c != ']' && c != ',') {
+                    int num = Character.getNumericValue(c);
+                    pair.packets.get(i).get(arrayIndex).add(num);
+                }
+            }
+        } 
+        return pair;
+    }
+    
+    
+    static void comparePackets(ArrayList<String> packets, ArrayList<Integer> indices) {
+        int i = 0;
+        int pairIndex = 1;
+        while (i < packets.size()) {
+            String left = packets.get(i);
+            String right = packets.get(i + 1);
+            Pair pair = parsePair(new String[] {left, right});
+            for (int j = 0; j < packets.get(i).length(); j++) {
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    static void parser(ArrayList<String> inputs) {
+        ArrayList<PacketPair> packets = new ArrayList<>();
+        for (int pairIndex = 0; pairIndex < 2; pairIndex++) {
+            for (int i = 0; i < inputs.size(); i++) {
+                String input = inputs.get(i);
+                PacketPair packet = new PacketPair();
+                int arrayIndex = 0;
+                
+                for (int j = 0; j < input.length(); j++) {
+                    char c = input.charAt(j);
+                    if (c == '[') {
+                        //packet.pair.get(pairIndex).get(arrayIndex).add(new ArrayList<Integer>());
+                    }
+                }
+            }
+            
+        }
+
+    }
+
+
+}
+
+class Pair {
+    int leftIndex = 0;
+    int rightIndex = 0;
+    ArrayList<ArrayList<ArrayList<Integer>>> packets = new ArrayList<ArrayList<ArrayList<Integer>>>();
+    
+    public Pair() {
+        packets.add(new ArrayList<ArrayList<Integer>>());
+        packets.add(new ArrayList<ArrayList<Integer>>());
+    }
+
+}
+
+
+class PacketPair {
+    ArrayList<ArrayList<Object>> pair = new ArrayList<>();
+
+    public PacketPair() {
+        pair.add(new ArrayList<>());
+        pair.add(new ArrayList<>());
+    }
+    
+}
---
+++ AOC2022/day15.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class day15 {
+    public static void main(String[] args) {
+        ArrayList<String> input = readInputFile();
+
+        buildGraph(input);
+    }
+
+    static ArrayList<String> readInputFile() {
+        ArrayList<String> strings = new ArrayList<String>();
+        try {
+            // read input file
+            File file = new File("input/day14_input.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                // add each line to a list of pairs
+                String row = scanner.nextLine();
+                strings.add(row);
+            }
+            scanner.close();
+        // if file couldnt be read print out error mgs
+        } catch (FileNotFoundException e) {
+            System.out.println("File could not be read.");
+            e.printStackTrace();
+        }
+        return strings;
+    }
+
+    static int[][] buildGraph(ArrayList<String> input) {
+        int[][] graph = new int[4_000_000][4_000_000];
+
+        for (int i = 0; i < input.size(); i++) {
+            String[] points = input.get(i).split(" ");
+            
+            int sensorX = Integer.parseInt(points[2].substring(2, points[2].length()-1));
+            int sensorY = Integer.parseInt(points[3].substring(2, points[3].length()-1));
+            Point sensor = new Point(sensorX, sensorY);
+            
+        }
+
+        return graph;
+    }
+}
+
+
+class Point {
+    int x;
+    int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
