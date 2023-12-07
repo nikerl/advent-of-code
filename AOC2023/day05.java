45a394edef8f5cb926202ef736d189f933114f2e
Nik Erlandsson <nik.erlandsson@gmail.com>
2023-12-07 17:17:53 +0100

    Day 5 part 2

--- AOC2023/day05.java
+++ AOC2023/day05.java
@@ -1,6 +1,5 @@
-import java.lang.reflect.Array;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class day05 {
     public static void main(String[] args) {
@@ -9,151 +8,123 @@ public class day05 {
         long part1 = part1(input);
         System.out.println("Part 1: " + part1);
 
+        System.out.println(LocalDateTime.now());
         long part2 = part2(input);
         System.out.println("Part 2: " + part2);
+        System.out.println(LocalDateTime.now());
         
     }
     
-    
-    static long part1(ArrayList<String> input) {
-        int index = 0;
-    
+    static long part1(ArrayList<String> input) {    
         ArrayList<Long> seeds = new ArrayList<Long>();
-        String[] tempSeeds = input.get(index).split(" ");
-        index += 3;
+        String[] tempSeeds = input.get(0).split(" ");
         for (int i = 1; i < tempSeeds.length; i++) {
             seeds.add(Long.parseLong(tempSeeds[i]));
         }
 
-        long min = getMinLocation(input, seeds, index);
+        ArrayList<ArrayList<ArrayList<Long>>> almanac = parseAlmanac(input);
+
+        long min = getMinLocation1(almanac, seeds);
         return min;      
     }
 
     static long part2(ArrayList<String> input) {
-        /* int index = 0;
-
-        ArrayList<Long> seeds = new ArrayList<Long>();
-        String[] tempSeeds = input.get(index).split(" ");
-        index += 3;
-        for (int i = 1; i < tempSeeds.length; i++) {
-            long startRange = Long.parseLong(tempSeeds[i++]);
-            long rangeLength = Long.parseLong(tempSeeds[i]);
-            for (long j = startRange; j < startRange + rangeLength; j++) {
-                seeds.add(j);
-            }
-        }
-
-        long min = getMinLocation(input, seeds, index);
-        return min; */
-
         int index = 0;
+        ArrayList<ArrayList<ArrayList<Long>>> almanac = parseAlmanac(input);
     
-        ArrayList<Long> seeds = new ArrayList<Long>();
-        String[] tempSeeds = input.get(index).split(" ");
-        index += 3;
-        for (int i = 1; i < tempSeeds.length; i++) {
-            seeds.add(Long.parseLong(tempSeeds[i]));
-        }
+        ArrayList<SeedRange> seeds = parseSeeds(input.get(0));
 
-        long min = getMinLocation(input, seeds, index);
+        long min = getMinLocation2(almanac, seeds, index);
         return min;    
     }
+    
+
+    static long getMinLocation1(ArrayList<ArrayList<ArrayList<Long>>> input, ArrayList<Long> seeds) {
+        long minLocation = Long.MAX_VALUE;
+        for (long seed : seeds) {
+            long tempSeed = seed;
+            for (ArrayList<ArrayList<Long>> map : input) {    
+                tempSeed = map(map, tempSeed);
+            }
+            if (tempSeed < minLocation) {
+                minLocation = tempSeed;
+            }
+
+        }
+        return minLocation;
+    }
 
 
-    static long getMinLocation(ArrayList<String> input, ArrayList<Long> seeds, int index) {
+    static long getMinLocation2(ArrayList<ArrayList<ArrayList<Long>>> input, ArrayList<SeedRange> seeds, int index) {
+        long minLocation = Long.MAX_VALUE;
+        for (SeedRange seedRange : seeds) {
+
+            for (long seed = seedRange.startSeed; seed < seedRange.startSeed + seedRange.rangeLength + 1; seed++) {
+                long tempSeed = seed;
+                for (ArrayList<ArrayList<Long>> map : input) {    
+                    tempSeed = map(map, tempSeed);
+                }
+                if (tempSeed < minLocation) {
+                    minLocation = tempSeed;
+                }
+            }
+        }
+        return minLocation;
+    }
+
+    static long map(ArrayList<ArrayList<Long>> map, long seed) {  
+        for (ArrayList<Long> line : map) {
+            long destinationRange = line.get(0);
+            long sourceRange = line.get(1);
+            long rangeLength = line.get(2);
+
+            if (seed >= sourceRange && seed < sourceRange + rangeLength) {
+                seed = seed - sourceRange + destinationRange;
+                return seed;
+            }     
+        }
+
+        return seed;
+    }
+
+    static ArrayList<SeedRange> parseSeeds(String input) {
+        ArrayList<SeedRange> seeds = new ArrayList<SeedRange>();
+        String[] tempSeeds = input.split(" ");
+        for (int i = 1; i < tempSeeds.length; i++) {
+            seeds.add(new SeedRange(Long.parseLong(tempSeeds[i++]), Long.parseLong(tempSeeds[i])));
+        }
+        return seeds;
+    }
+
+    static ArrayList<ArrayList<ArrayList<Long>>> parseAlmanac(ArrayList<String> input) {
+        ArrayList<ArrayList<ArrayList<Long>>> almanac = new ArrayList<ArrayList<ArrayList<Long>>>();
+        int index = 3;
+
         while (index < input.size()) {
-            ArrayList<String> map = new ArrayList<String>();
+            ArrayList<ArrayList<Long>> map = new ArrayList<ArrayList<Long>>();
             while (index < input.size() && input.get(index).length() != 0) {
-                map.add(input.get(index));
+                String[] ranges = input.get(index).split(" ");
+                ArrayList<Long> line = new ArrayList<Long>();
+                line.add(Long.parseLong(ranges[0])); //destinationRange
+                line.add(Long.parseLong(ranges[1])); //sourceRange
+                line.add(Long.parseLong(ranges[2])); //rangeLength
+                map.add(line);
                 index++;
             }
+            almanac.add(map);
             index += 2;
-    
-            ArrayList<Long> mappedSeeds = map(map, seeds);
-            seeds = mappedSeeds;
         }
-    
-        long min = Long.MAX_VALUE;
-        for (long seed : seeds) {
-            if (seed < min) {
-                min = seed;
-            }
-        }
-    
-        return min;
+
+        return almanac;
     }
+}
 
+class SeedRange {
+    long startSeed;
+    long rangeLength;
 
-    static ArrayList<Long> map(ArrayList<String> map, ArrayList<Long> seeds) {  
-        ArrayList<Long> mappedSeeds = new ArrayList<Long>();
-
-        for (String line : map) {
-            String[] ranges = line.split(" ");
-            long destinationRange = Long.parseLong(ranges[0]);
-            long sourceRange = Long.parseLong(ranges[1]);
-            long rangeLength = Long.parseLong(ranges[2]);
-
-            Iterator<Long> i = seeds.iterator();
-            while (i.hasNext()) {
-                long seed = i.next();
-                if (seed >= sourceRange && seed <= sourceRange + rangeLength) {
-                    long mappedSeed = seed - sourceRange + destinationRange;
-                    mappedSeeds.add(mappedSeed);
-                    i.remove();
-                }
-            }
-        }
-
-        if (seeds.size() > 0) {
-            for (long seed : seeds) {
-                mappedSeeds.add(seed);
-            }
-        }
-
-        return mappedSeeds;
-    }
-
-    static ArrayList<Long> map2(ArrayList<String> map, ArrayList<Long> seeds) {  
-        ArrayList<Long> mappedSeeds = new ArrayList<Long>();
-        ArrayList<ArrayList<Long>> mapList = new ArrayList<ArrayList<Long>>();
-
-        for (int i = 0; i < map.size(); i++) {
-            String line = map.get(i);
-            mapList.add(new ArrayList<Long>());
-            String[] ranges = line.split(" ");
-            mapList.get(i).add(Long.parseLong(ranges[0])); // destination range
-            mapList.get(i).add(Long.parseLong(ranges[1])); // source range
-            mapList.get(i).add(Long.parseLong(ranges[2])); // range length
-        }
-
-        
-        for (int i = 0; i < seeds.size(); i++) {
-            long seed = seeds.get(i++);
-            long seedLength = seeds.get(i);
-
-            for (int j = 0; j < mapList.size(); j++) {
-                long destinationRange = mapList.get(j).get(0);
-                long sourceRange = mapList.get(j).get(1);
-                long rangeLength = mapList.get(j).get(2);
-
-                if (seed >= sourceRange && seed <= sourceRange + rangeLength) {
-                    long mappedSeed = seed - sourceRange + destinationRange;
-                    mappedSeeds.add(mappedSeed);
-                }
-                if (seed + seedLength > sourceRange + rangeLength) {
-                    long mappedSeed = seed;
-                    mappedSeeds.add(mappedSeed);
-                }
-            }
-        }
-
-
-        if (seeds.size() > 0) {
-            for (long seed : seeds) {
-                mappedSeeds.add(seed);
-            }
-        }
-
-        return mappedSeeds;
+    public SeedRange(long startSeed, long rangeLength) {
+        this.startSeed = startSeed;
+        this.rangeLength = rangeLength;
     }
 }
