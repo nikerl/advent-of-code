e71088ab1f00ce64ad1635cc8133a6ab06666cd6
Nik Erlandsson <44620116+nikerl@users.noreply.github.com>
2024-12-15 14:50:50 +0100

    Day 14 part 1

---
+++ AOC2024/day14.cpp
@@ -0,0 +1,126 @@
+#include <cmath>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+#define MAP_X_SIZE 101
+#define MAP_Y_SIZE 103
+
+struct RobotVector {
+    int x;
+    int y;
+    int Vx;
+    int Vy;
+};
+
+vector<string> split (const string &s, char delim) {
+    vector<string> result;
+    stringstream ss (s);
+    string item;
+
+    while (getline (ss, item, delim)) {
+        result.push_back (item);
+    }
+
+    return result;
+}
+
+vector<string> readFile(string path) {
+    ifstream file(path);
+    vector<string> lines;
+    string line;
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            lines.push_back(line);
+        }
+        file.close();
+    } else {
+        cout << "Unable to open file" << endl;
+    }
+
+    return lines;
+}
+
+vector<RobotVector> parseInput(vector<string> input) {
+    vector<RobotVector> robots;
+    for (string line : input) {
+        vector<string> substr = split(line, ' ');
+        vector<string> pos = split(split(substr[0], '=')[1], ',');
+        vector<string> vel = split(split(substr[1], '=')[1], ',');
+        robots.push_back({stoi(pos[0]), stoi(pos[1]), stoi(vel[0]), stoi(vel[1])});
+    }
+    return robots;
+}
+
+inline int positiveModulo(int value, int modulus) {
+    return (value % modulus + modulus) % modulus;
+}
+
+void moveRobot(RobotVector *robot) {
+    robot->x = positiveModulo(robot->x + robot->Vx, MAP_X_SIZE);
+    robot->y = positiveModulo(robot->y + robot->Vy, MAP_Y_SIZE);
+}
+
+int calculateSafetyFactor(vector<RobotVector> *robots) {
+    int xpivot = MAP_X_SIZE / 2;
+    int ypivot = MAP_Y_SIZE / 2;
+
+    vector<int> quadrants = {0, 0, 0, 0};
+    for (int i = 0; i < robots->size(); i++) {
+        RobotVector robot = robots->at(i);
+        
+        // Quadrant 1
+        if ((robot.x >= 0 && robot.x < xpivot) && (robot.y >= 0 && robot.y < ypivot)) {
+            quadrants[0]++;
+        } 
+        // Quadrant 2
+        else if ((robot.x > xpivot && robot.x < MAP_X_SIZE) && (robot.y >= 0 && robot.y < ypivot)) {
+            quadrants[1]++;
+        }
+        // Quadrant 3
+        if ((robot.x >= 0 && robot.x < xpivot) && (robot.y > ypivot && robot.y < MAP_Y_SIZE)) {
+            quadrants[2]++;
+        } 
+        // Quadrant 4
+        else if ((robot.x > xpivot && robot.x < MAP_X_SIZE) && (robot.y > ypivot && robot.y < MAP_Y_SIZE)) {
+            quadrants[3]++;
+        }
+    }
+
+    int safetyFactor = 1;
+    for (int i = 0; i < quadrants.size(); i++) {
+        if (quadrants[i] != 0) {
+            safetyFactor *= quadrants[i];
+        }
+    }
+
+    return safetyFactor;
+}
+
+
+int part1(vector<RobotVector> robots, int seconds) {
+    for (int i = 0; i < seconds; i++) {
+        for (int j = 0; j < robots.size(); j++) {
+            moveRobot(&robots[j]);
+        }
+    }
+    int safetyFactor = calculateSafetyFactor(&robots);
+    cout << "Part 1 result: " << safetyFactor << endl;
+
+    return 0;
+}
+
+int main() {
+    vector<string> input = readFile("/home/nikerl/Documents/Repos/advent-of-code/AOC2024/input/day14_input.txt");
+    vector<RobotVector> robots = parseInput(input);
+    
+    part1(robots, 100);
+
+    return 0;
+}
