8a0babeb46dc4fd914614c2a84d77b06fbf0fcb4
Nik Erlandsson <44620116+nikerl@users.noreply.github.com>
2025-12-08 16:43:53 +0100

    Add SplitString and TransposeMatrix functions

--- AOC2025/lib/lib.go
+++ AOC2025/lib/lib.go
@@ -38,3 +38,44 @@ func Log10(n int) int {
 	}
 	return log
 }
+
+// Splits a string into substrings separated by the given delimiter rune.
+//
+// Ignores consecutive delimiters and does not include empty substrings in the result.
+func SplitString(str string, delimiter rune) []string {
+	var result []string
+	var stringRune []rune = []rune(str)
+	for i := 0; i < len(str); i++ {
+		if stringRune[i] != delimiter {
+			var substr []rune = []rune{}
+			for i < len(str) && stringRune[i] != delimiter {
+				substr = append(substr, stringRune[i])
+				i++
+			}
+			result = append(result, string(substr))
+		}
+	}
+	return result
+}
+
+// Transposes a 2D matrix.
+//
+// Accepts matrixes with elements of any type.
+func TransposeMatrix[T any](matrix [][]T) [][]T {
+    rows := len(matrix)
+    cols := len(matrix[0])
+    result := make([][]T, cols)
+
+    for i := range result {
+        result[i] = make([]T, rows)
+    }
+
+    for i := 0; i < rows; i++ {
+        for j := 0; j < cols; j++ {
+            result[j][i] = matrix[i][j]
+        }
+    }
+
+    return result
+}
+
